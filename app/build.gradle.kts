9d8fa8119b778016a3b46a0f96c7660705ee3b84
Jamiro <jamirocristian15@gmail.com>
2023-10-11 20:18:24 -0300

    navigation components pt2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.jamirodev.horoscopoapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.jamirodev.horoscopoapp"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
