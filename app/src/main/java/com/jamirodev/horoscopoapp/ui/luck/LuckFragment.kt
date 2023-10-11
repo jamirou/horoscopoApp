cba541998f33dcade39d3585cf66cbe66be29fdf
Jamiro <jamirocristian15@gmail.com>
2023-10-11 10:28:27 -0300

    new binding

--- app/src/main/java/com/jamirodev/horoscopoapp/ui/luck/LuckFragment.kt
+++ app/src/main/java/com/jamirodev/horoscopoapp/ui/luck/LuckFragment.kt
@@ -1,22 +1,23 @@
-package com.jamirodev.horoscopoapp.ui.horoscope
+package com.jamirodev.horoscopoapp.ui.luck
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import com.jamirodev.horoscopoapp.R
 import com.jamirodev.horoscopoapp.databinding.FragmentHoroscopeBinding
+import com.jamirodev.horoscopoapp.databinding.FragmentLuckBinding
 
-class HoroscopeFragment : Fragment() {
+class LuckFragment : Fragment() {
 
-    private var _binding: FragmentHoroscopeBinding? = null
+    private var _binding: FragmentLuckBinding? = null
     private val binding get() = _binding!!
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentHoroscopeBinding.inflate(layoutInflater, container, false)
+        _binding = FragmentLuckBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
