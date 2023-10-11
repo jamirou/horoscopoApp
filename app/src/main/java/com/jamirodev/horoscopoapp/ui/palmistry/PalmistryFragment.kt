8ede6093226580f00c735c32b895db96a292eb3d
Jamiro <jamirocristian15@gmail.com>
2023-10-11 10:34:20 -0300

    adding palmistry fragment

--- app/src/main/java/com/jamirodev/horoscopoapp/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/jamirodev/horoscopoapp/ui/palmistry/PalmistryFragment.kt
@@ -1,21 +1,21 @@
-package com.jamirodev.horoscopoapp.ui.luck
+package com.jamirodev.horoscopoapp.ui.palmistry
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.jamirodev.horoscopoapp.databinding.FragmentLuckBinding
+import com.jamirodev.horoscopoapp.databinding.FragmentPalmistryBinding
 
-class LuckFragment : Fragment() {
-
-    private var _binding: FragmentLuckBinding? = null
+class PalmistryFragment : Fragment() {
+    private var _binding: FragmentPalmistryBinding? = null
     private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentLuckBinding.inflate(layoutInflater, container, false)
+        _binding = FragmentPalmistryBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
