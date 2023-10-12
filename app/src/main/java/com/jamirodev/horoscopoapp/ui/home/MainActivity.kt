abf31c495c11c2567e75094719cc07a864fecac2
Jamiro <jamirocristian15@gmail.com>
2023-10-12 18:52:08 -0300

    adding navigation

--- app/src/main/java/com/jamirodev/horoscopoapp/ui/home/MainActivity.kt
+++ app/src/main/java/com/jamirodev/horoscopoapp/ui/home/MainActivity.kt
@@ -2,13 +2,30 @@ package com.jamirodev.horoscopoapp.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.jamirodev.horoscopoapp.R
 import com.jamirodev.horoscopoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding:ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initUI()
+    }
+
+    private fun initUI() {
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNavView.setupWithNavController(navController)
     }
 }
