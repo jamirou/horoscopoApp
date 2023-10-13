27770166a4b2624be88dadd213812adcd601335b
Jamiro <jamirocristian15@gmail.com>
2023-10-13 19:08:27 -0300

    adding connection with stateflow and fragment to model

--- app/src/main/java/com/jamirodev/horoscopoapp/ui/horoscope/HoroscopeFragment.kt
+++ app/src/main/java/com/jamirodev/horoscopoapp/ui/horoscope/HoroscopeFragment.kt
@@ -1,13 +1,19 @@
 package com.jamirodev.horoscopoapp.ui.horoscope
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.jamirodev.horoscopoapp.databinding.FragmentHoroscopeBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HoroscopeFragment : Fragment() {
@@ -15,6 +21,24 @@ class HoroscopeFragment : Fragment() {
     private val horoscopeViewModel by viewModels<HoroscopeViewModel>()
     private var _binding: FragmentHoroscopeBinding? = null
     private val binding get() = _binding!!
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUI()
+    }
+
+    private fun initUI() {
+        initUIState()
+    }
+
+    private fun initUIState() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                horoscopeViewModel.horoscope.collect{
+                    Log.i("Jamiro", it.toString())
+                }
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
--- app/src/main/java/com/jamirodev/horoscopoapp/ui/horoscope/HoroscopeViewModel.kt
+++ app/src/main/java/com/jamirodev/horoscopoapp/ui/horoscope/HoroscopeViewModel.kt
@@ -1,9 +1,22 @@
 package com.jamirodev.horoscopoapp.ui.horoscope
 
 import androidx.lifecycle.ViewModel
+import com.jamirodev.horoscopoapp.domain.model.HoroscopeInfo
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class HoroscopeViewModel @Inject constructor() : ViewModel() {
+
+    private var _horoscope = MutableStateFlow<List<HoroscopeInfo>>(emptyList())
+    val horoscope: StateFlow<List<HoroscopeInfo>> = _horoscope
+
+    init {
+        _horoscope.value =
+            listOf(
+                HoroscopeInfo.Aries, HoroscopeInfo.Taurus, HoroscopeInfo.Geminis
+            )
+    }
 }
