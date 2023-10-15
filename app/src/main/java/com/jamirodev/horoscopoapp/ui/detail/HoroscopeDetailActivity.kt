b7d3e9dd0ff5c4c76d64ca1383b42b4c9008b504
Jamiro <jamirocristian15@gmail.com>
2023-10-15 11:06:04 -0300

    fixing navigation

--- app/src/main/java/com/jamirodev/horoscopoapp/ui/detail/HoroscopeDetailActivity.kt
+++ app/src/main/java/com/jamirodev/horoscopoapp/ui/detail/HoroscopeDetailActivity.kt
@@ -4,18 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import com.jamirodev.horoscopoapp.R
+import com.jamirodev.horoscopoapp.databinding.ActivityHoroscopeDetailBinding
 import com.jamirodev.horoscopoapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HoroscopeDetailActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityHoroscopeDetailBinding
     private val horoscopeDetailViewModel:HoroscopeDetailViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityHoroscopeDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
--- app/src/main/java/com/jamirodev/horoscopoapp/ui/horoscope/HoroscopeFragment.kt
+++ app/src/main/java/com/jamirodev/horoscopoapp/ui/horoscope/HoroscopeFragment.kt
@@ -4,12 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.jamirodev.horoscopoapp.databinding.FragmentHoroscopeBinding
 import com.jamirodev.horoscopoapp.ui.horoscope.adapter.HoroscopeAdapter
@@ -36,7 +36,9 @@ class HoroscopeFragment : Fragment() {
 
     private fun initList() {
         horoscopeAdapter = HoroscopeAdapter(onItemSelected = {
-            Toast.makeText(context, getString(it.name), Toast.LENGTH_LONG).show()
+            findNavController().navigate(
+                HoroscopeFragmentDirections.actionHoroscopeFragmentToHoroscopeDetailActivity()
+            )
         })
 
         binding.rvHoroscope.apply {
