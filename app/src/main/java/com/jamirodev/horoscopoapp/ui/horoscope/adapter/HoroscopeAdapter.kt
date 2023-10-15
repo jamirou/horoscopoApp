f2a4af3101cd69e70f865ea02b4cc3dc03b91a87
Jamiro <jamirocristian15@gmail.com>
2023-10-15 07:53:20 -0300

    adding rv

---
+++ app/src/main/java/com/jamirodev/horoscopoapp/ui/horoscope/adapter/HoroscopeAdapter.kt
@@ -0,0 +1,22 @@
+package com.jamirodev.horoscopoapp.ui.horoscope.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.jamirodev.horoscopoapp.R
+import com.jamirodev.horoscopoapp.domain.model.HoroscopeInfo
+
+class HoroscopeAdapter(private var horoscopeList: List<HoroscopeInfo> = emptyList()) :
+    RecyclerView.Adapter<HoroscopeViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HoroscopeViewHolder {
+        return HoroscopeViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_horoscope, parent, false)
+        )
+    }
+
+    override fun getItemCount() = horoscopeList.size
+
+    override fun onBindViewHolder(holder: HoroscopeViewHolder, position: Int) {
+        holder.render(horoscopeList[position])
+    }
+}
---
+++ app/src/main/java/com/jamirodev/horoscopoapp/ui/horoscope/adapter/HoroscopeViewHolder.kt
@@ -0,0 +1,15 @@
+package com.jamirodev.horoscopoapp.ui.horoscope.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.jamirodev.horoscopoapp.databinding.ItemHoroscopeBinding
+import com.jamirodev.horoscopoapp.domain.model.HoroscopeInfo
+
+class HoroscopeViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    private val binding = ItemHoroscopeBinding.bind(view)
+
+    fun render(horoscopeInfo: HoroscopeInfo) {
+
+    }
+}
