458580ccfc33deb561936be330635de7962f62d8
Jamiro <jamirocristian15@gmail.com>
2023-10-16 20:25:30 -0300

    adding logic for 3 states

--- app/src/main/java/com/jamirodev/horoscopoapp/ui/detail/HoroscopeDetailActivity.kt
+++ app/src/main/java/com/jamirodev/horoscopoapp/ui/detail/HoroscopeDetailActivity.kt
@@ -3,9 +3,15 @@ package com.jamirodev.horoscopoapp.ui.detail
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.navArgs
 import com.jamirodev.horoscopoapp.databinding.ActivityHoroscopeDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HoroscopeDetailActivity : AppCompatActivity() {
@@ -17,6 +23,31 @@ class HoroscopeDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHoroscopeDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        args.type
+        initUI()
+    }
+
+    private fun initUI() {
+        initUIState()
+    }
+
+    private fun initUIState() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                horoscopeDetailViewModel.state.collect{
+                    when(it){
+                        HoroscopeDetailState.Loading -> loadingState()
+                        is HoroscopeDetailState.Error -> errorState()
+                        is HoroscopeDetailState.Success -> successState()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun loadingState() {
+    }
+    private fun errorState() {
+    }
+    private fun successState(){
     }
 }
--- app/src/main/java/com/jamirodev/horoscopoapp/ui/detail/HoroscopeDetailViewModel.kt
+++ app/src/main/java/com/jamirodev/horoscopoapp/ui/detail/HoroscopeDetailViewModel.kt
@@ -2,9 +2,13 @@ package com.jamirodev.horoscopoapp.ui.detail
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class HoroscopeDetailViewModel @Inject constructor(): ViewModel() {
 
+    private var _state = MutableStateFlow<HoroscopeDetailState>(HoroscopeDetailState.Loading)
+    val state:StateFlow<HoroscopeDetailState> = _state
 }
