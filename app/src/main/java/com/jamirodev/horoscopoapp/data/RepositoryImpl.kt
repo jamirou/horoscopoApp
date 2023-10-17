1c72231a96af4127104c721e36ea987be6596803
Jamiro <jamirocristian15@gmail.com>
2023-10-17 20:10:23 -0300

    adding networkmodule

--- app/src/main/java/com/jamirodev/horoscopoapp/data/RepositoryImpl.kt
+++ app/src/main/java/com/jamirodev/horoscopoapp/data/RepositoryImpl.kt
@@ -1,9 +1,10 @@
 package com.jamirodev.horoscopoapp.data
 
 import com.jamirodev.horoscopoapp.domain.Repository
+import javax.inject.Inject
 
-class RepositoryImpl:Repository {
+class RepositoryImpl @Inject constructor() :Repository {
     override suspend fun getPrediction(sign: String) {
-        
+
     }
 }
---
+++ app/src/main/java/com/jamirodev/horoscopoapp/data/network/NetworkModule.kt
@@ -0,0 +1,24 @@
+package com.jamirodev.horoscopoapp.data.network
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit
+            .Builder()
+            .baseUrl("https://newastro.vercel.app/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
