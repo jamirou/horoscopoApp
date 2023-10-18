63a8d0eda44850ddd230b20baca72d95fdcfb6fb
Jamiro <jamirocristian15@gmail.com>
2023-10-18 10:26:57 -0300

    adding interceptor

--- app/src/main/java/com/jamirodev/horoscopoapp/data/network/NetworkModule.kt
+++ app/src/main/java/com/jamirodev/horoscopoapp/data/network/NetworkModule.kt
@@ -6,6 +6,8 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -16,12 +18,24 @@ object NetworkModule {
 
     @Provides
     @Singleton
-    fun provideRetrofit(): Retrofit {
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
         return Retrofit
             .Builder()
             .baseUrl("https://newastro.vercel.app/")
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient():OkHttpClient{
+        val interceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        return OkHttpClient
+            .Builder()
+            .addInterceptor(interceptor)
+            .build()
+
     }
 
     @Provides
