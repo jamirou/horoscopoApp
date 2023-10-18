0b47f4e650af3e612a0bfcff995588d1eb23f674
Jamiro <jamirocristian15@gmail.com>
2023-10-18 10:35:03 -0300

    adding interceptor

---
+++ app/src/main/java/com/jamirodev/horoscopoapp/data/core/interceptors/AuthInterceptor.kt
@@ -0,0 +1,10 @@
+package com.jamirodev.horoscopoapp.data.core.interceptors
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthInterceptor:Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        
+    }
+}
