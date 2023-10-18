39e8bba1578eec9efe2f564741e79a8184505026
Jamiro <jamirocristian15@gmail.com>
2023-10-18 09:08:25 -0300

    using usecase

--- app/src/main/java/com/jamirodev/horoscopoapp/domain/usecase/GetPredictionUseCase.kt
+++ app/src/main/java/com/jamirodev/horoscopoapp/domain/usecase/GetPredictionUseCase.kt
@@ -4,5 +4,5 @@ import com.jamirodev.horoscopoapp.domain.Repository
 import javax.inject.Inject
 
 class GetPredictionUseCase @Inject constructor(private val repository: Repository) {
-
+    suspend operator fun invoke(sign: String) = repository.getPrediction(sign)
 }
--- app/src/main/java/com/jamirodev/horoscopoapp/ui/detail/HoroscopeDetailViewModel.kt
+++ app/src/main/java/com/jamirodev/horoscopoapp/ui/detail/HoroscopeDetailViewModel.kt
@@ -1,14 +1,19 @@
 package com.jamirodev.horoscopoapp.ui.detail
 
 import androidx.lifecycle.ViewModel
+import com.jamirodev.horoscopoapp.domain.usecase.GetPredictionUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class HoroscopeDetailViewModel @Inject constructor(): ViewModel() {
+class HoroscopeDetailViewModel @Inject constructor(private val getPredictionUseCase: GetPredictionUseCase): ViewModel() {
 
     private var _state = MutableStateFlow<HoroscopeDetailState>(HoroscopeDetailState.Loading)
     val state:StateFlow<HoroscopeDetailState> = _state
+
+    fun example() {
+        getPredictionUseCase()
+    }
 }
