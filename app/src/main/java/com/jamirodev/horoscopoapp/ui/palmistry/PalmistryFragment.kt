799c29f073527add8ca83255713aebc51b6bbcef
Jamiro <jamirocristian15@gmail.com>
2023-10-20 09:21:30 -0300

    added camera

--- app/src/main/java/com/jamirodev/horoscopoapp/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/jamirodev/horoscopoapp/ui/palmistry/PalmistryFragment.kt
@@ -2,11 +2,16 @@ package com.jamirodev.horoscopoapp.ui.palmistry
 
 import android.Manifest
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.core.content.PermissionChecker
 import androidx.fragment.app.Fragment
 import com.jamirodev.horoscopoapp.databinding.FragmentPalmistryBinding
@@ -26,7 +31,7 @@ class PalmistryFragment : Fragment() {
         ActivityResultContracts.RequestPermission()
     ) { isGranted ->
         if (isGranted) {
-
+            startCamera()
         } else {
             Toast.makeText(
                 requireContext(), "Please accept permission", Toast.LENGTH_LONG
@@ -37,10 +42,33 @@ class PalmistryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         if (checkCameraPermission()) {
-
+            startCamera()
         } else {
             requestPermissionLauncher.launch(CAMERA_PERMISSIONS)
         }
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener({
+            val cameraProvider:ProcessCameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+            }catch (e:Exception) {
+                Log.e("Jamiro", "Something went wrong, ${e.message}")
+            }
+        }, ContextCompat.getMainExecutor(requireContext()) )
     }
 
     private fun checkCameraPermission(): Boolean {
