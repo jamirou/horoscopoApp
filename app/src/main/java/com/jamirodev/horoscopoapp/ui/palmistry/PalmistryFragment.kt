a7a516ede3489fb86048b4ee2a7c8a1f5b943893
Jamiro <jamirocristian15@gmail.com>
2023-10-20 08:45:35 -0300

    sking user permissions pt1

--- app/src/main/java/com/jamirodev/horoscopoapp/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/jamirodev/horoscopoapp/ui/palmistry/PalmistryFragment.kt
@@ -1,9 +1,11 @@
 package com.jamirodev.horoscopoapp.ui.palmistry
 
+import android.Manifest
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.PermissionChecker
 import androidx.fragment.app.Fragment
 import com.jamirodev.horoscopoapp.databinding.FragmentPalmistryBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -15,7 +17,18 @@ class PalmistryFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        
+        if (checkCameraPermission()) {
+
+        }else {
+
+        }
+    }
+
+    private fun checkCameraPermission():Boolean {
+        return PermissionChecker.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.CAMERA
+        ) == PermissionChecker.PERMISSION_GRANTED
     }
 
     override fun onCreateView(
