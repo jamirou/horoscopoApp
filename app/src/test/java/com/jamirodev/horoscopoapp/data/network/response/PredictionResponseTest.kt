9f12d5ead2d805cdc6c74fc403158ab9233387ab
Jamiro <jamirocristian15@gmail.com>
2023-10-20 10:21:47 -0300

    adding generic response

--- app/src/test/java/com/jamirodev/horoscopoapp/data/network/response/PredictionResponseTest.kt
+++ app/src/test/java/com/jamirodev/horoscopoapp/data/network/response/PredictionResponseTest.kt
@@ -1,5 +1,8 @@
 package com.jamirodev.horoscopoapp.data.network.response
 
+import com.jamirodev.horoscopoapp.motherobject.HoroscopeMotherObject
+import com.jamirodev.horoscopoapp.motherobject.HoroscopeMotherObject.anyResponse
+import io.kotlintest.shouldBe
 import org.junit.Assert.*
 import org.junit.Test
 
@@ -8,9 +11,11 @@ class PredictionResponseTest{
     @Test
     fun `toDomain should return a correct PredictionModel`() {
         //Given
-        
+        val horoscopeResponse = anyResponse
         //When
-
+        val predictionModel = horoscopeResponse.toDomain()
         //Then
+        predictionModel.sign shouldBe  horoscopeResponse.sign
+        predictionModel.horoscope shouldBe horoscopeResponse.horoscope
     }
 }
---
+++ app/src/test/java/com/jamirodev/horoscopoapp/motherobject/HoroscopeMotherObject.kt
@@ -0,0 +1,8 @@
+package com.jamirodev.horoscopoapp.motherobject
+
+import com.jamirodev.horoscopoapp.data.network.response.PredictionResponse
+
+object HoroscopeMotherObject {
+
+    val anyResponse = PredictionResponse("date", "prediction", "taurus")
+}
