4f06321ea4afaa5bdc94ad69e3244b39ba873e74
Jamiro <jamirocristian15@gmail.com>
2023-10-20 10:02:30 -0300

    adding var for unitTst pt1

--- app/src/test/java/com/jamirodev/horoscopoapp/data/network/response/PredictionResponseTest.kt
+++ app/src/test/java/com/jamirodev/horoscopoapp/data/network/response/PredictionResponseTest.kt
@@ -1,7 +1,16 @@
 package com.jamirodev.horoscopoapp.data.network.response
 
 import org.junit.Assert.*
+import org.junit.Test
 
 class PredictionResponseTest{
-    
+
+    @Test
+    fun `toDomain should return a correct PredictionModel`() {
+        //Given
+        
+        //When
+
+        //Then
+    }
 }
