e9021bad9782cccc9b974d01aed210662716349b
Jamiro <jamirocristian15@gmail.com>
2023-10-20 09:46:30 -0300

    adding Unit testing runner

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,7 +82,11 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    //Unit Testing
     testImplementation("junit:junit:4.13.2")
+    testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
+
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
