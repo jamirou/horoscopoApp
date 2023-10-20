fc7996238ce3bb94c0c5d3f8536c5ed8e13739bf
Jamiro <jamirocristian15@gmail.com>
2023-10-20 10:58:35 -0300

    added mockk test

--- app/src/test/java/com/jamirodev/horoscopoapp/motherobject/HoroscopeMotherObject.kt
+++ app/src/test/java/com/jamirodev/horoscopoapp/motherobject/HoroscopeMotherObject.kt
@@ -1,8 +1,24 @@
 package com.jamirodev.horoscopoapp.motherobject
 
 import com.jamirodev.horoscopoapp.data.network.response.PredictionResponse
+import com.jamirodev.horoscopoapp.domain.model.HoroscopeInfo
 
 object HoroscopeMotherObject {
 
     val anyResponse = PredictionResponse("date", "prediction", "taurus")
+
+    val horoscopeInfoList = listOf(
+        HoroscopeInfo.Aries,
+        HoroscopeInfo.Taurus,
+        HoroscopeInfo.Geminis,
+        HoroscopeInfo.Cancer,
+        HoroscopeInfo.Leo,
+        HoroscopeInfo.Capricorn,
+        HoroscopeInfo.Scorpio,
+        HoroscopeInfo.Libra,
+        HoroscopeInfo.Aquarius,
+        HoroscopeInfo.Pisces,
+        HoroscopeInfo.Sagittarius,
+        HoroscopeInfo.Virgo
+    )
 }
--- app/src/test/java/com/jamirodev/horoscopoapp/ui/horoscope/HoroscopeViewModelTest.kt
+++ app/src/test/java/com/jamirodev/horoscopoapp/ui/horoscope/HoroscopeViewModelTest.kt
@@ -1,17 +1,33 @@
 package com.jamirodev.horoscopoapp.ui.horoscope
 
+import com.jamirodev.horoscopoapp.data.providers.HoroscopeProvider
+import com.jamirodev.horoscopoapp.motherobject.HoroscopeMotherObject
+import com.jamirodev.horoscopoapp.motherobject.HoroscopeMotherObject.horoscopeInfoList
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import junit.framework.TestCase.assertTrue
 import org.junit.Before
 import org.junit.Test
 
 class HoroscopeViewModelTest{
 
+    @MockK
+    lateinit var horoscopeProvider: HoroscopeProvider
+
     private lateinit var viewModel: HoroscopeViewModel
     @Before
     fun setUp() {
+        MockKAnnotations.init(this, relaxUnitFun = true)
     }
 
     @Test
     fun `when viewModel is created then horoscopes are created`() {
-        viewModel = HoroscopeViewModel()
+        every { horoscopeProvider.getHoroscopes() } returns horoscopeInfoList
+        viewModel = HoroscopeViewModel(horoscopeProvider)
+
+        val horoscopes = viewModel.horoscope.value
+
+        assertTrue(horoscopes.isNotEmpty())
     }
 }
